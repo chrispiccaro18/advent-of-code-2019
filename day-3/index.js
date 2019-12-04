5d28e795e21defb178b2015076674b3128364d6e
chrispiccaro <chrispiccaro18@gmail.com>
2019-12-03 18:33:25 -0800

    conditional city in here

--- day-3/index.js
+++ day-3/index.js
@@ -15,7 +15,6 @@ const determineDisplacement = (direction, distance) => {
   return result;
 };
 
-
 const start = {
   x: 0,
   y: 0
@@ -55,15 +54,46 @@ const secondPath = 'U4,R6';
 // would intersect at 2, 4 (x, y)
 
 const findIntersects = (firstPathPoints, secondPathPoints) => {
+  const intersections = [];
+  
   // compare two lines
   // aka two points from first path and two points from second path
-  // intersection is x of vertical and y of horizontal
-  // not an intersection if they are both horizontal or vertical
-  // not an intersection if x of intersection is not in horizontal line
-  // or y of intersection is not in vertical line
+  for(let i = 0; i < firstPathPoints.length - 1; i++) {
+    const firstPathLine = { point1: firstPathPoints[i], point2: firstPathPoints[i + 1] };
+    const secondPathLine = { point1: secondPathPoints[i], point2: secondPathPoints[i + 1] };
+    determineHorOrVert(firstPathLine);
+    determineHorOrVert(secondPathLine);
+
+    // not an intersection if they are both horizontal or vertical
+    if(firstPathLine.isVert === secondPathLine.isVert) continue;
+
+    // intersection is x of vertical and y of horizontal
+    let x, y;
+    if(firstPathLine.isVert) {
+      x = firstPathLine.point1.x;
+      y = secondPathLine.point1.y;
+      // not an intersection if x of intersection is not in horizontal line
+      // or y of intersection is not in vertical line
+
+
+    }
+
+    // const x = firstPathLine.isVert ? firstPathLine.point1.x : secondPathLine.point1.x;
+    // const y = firstPathLine.isVert ? secondPathLine.point1.y : firstPathLine.point1.y;
+    
+    intersections.push({ x, y });
+  }
+  return intersections;
+};
+
+const determineHorOrVert = line => {
+  const { point1, point2 } = line;
+  line.isVert = point1.x === point2.x;
 };
 
 const { firstPathPoints, secondPathPoints } = returnPathPoints(firstPath, secondPath);
+
+console.log(findIntersects(firstPathPoints, secondPathPoints));
 
 // The first key is that two lines can only cross if one is vertical and the other horizontal. When comparing vertical/vertical or horizontal/horizontal line segments, you can discard them.
 // The second key is to identify the vertical and horizontal line segments. The horizontal line has an unchanging Y value and the vertical line has an unchanging X value. Here is a rough sketch I made to illustrate.
