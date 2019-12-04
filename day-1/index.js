36fe43d8f5f9e6edb2f6c0c5dd635b7ad4badc8c
chrispiccaro <chrispiccaro18@gmail.com>
2019-12-03 17:50:51 -0800

    config and day 1

---
+++ day-1/index.js
@@ -0,0 +1,17 @@
+const string = require('./input');
+
+const fuelCounterUpper = mass => {
+  return Math.floor(mass / 3) - 2;
+};
+
+const calcTotalFuel = moduleArray => {
+  return moduleArray.reduce((prevValue, module) => {
+    return prevValue + fuelCounterUpper(module);
+  }, 0);
+}
+
+const array = string.split('\n').map(int => parseInt(int));
+
+const result = calcTotalFuel(array);
+
+console.log(result);
