a8d74a4d6d7fcc90391ebcc5aca5079bdeda818d
chrispiccaro <chrispiccaro18@gmail.com>
2019-12-05 20:23:27 -0800

    part 1 done

--- test/day-3/index.test.js
+++ test/day-3/index.test.js
@@ -1,4 +1,9 @@
-const { returnPathPoints } = require('../../lib/day-3/index');
+const {
+  returnPathPoints,
+  findIntersects,
+  calcDistanceOfClosestIntersect,
+  crossedWires,
+} = require('../../lib/day-3/index');
 
 describe('day 3', () => {
   it('returns path points', () => {
@@ -7,8 +12,47 @@ describe('day 3', () => {
 
     const result = returnPathPoints(firstPath, secondPath);
     expect(result).toEqual({
-      firstPathPoints: [{ x: 2, y: 0 }, { x: 2, y: 5 }],
-      secondPathPoints: [{ x: 0, y: 4 }, { x: 6, y: 4 }]
+      firstPathPoints: [{ x: 0, y: 0 }, { x: 2, y: 0 }, { x: 2, y: 5 }],
+      secondPathPoints: [{ x: 0, y: 0 }, { x: 0, y: 4 }, { x: 6, y: 4 }]
     });
+  });
+
+  it('returns the intersects', () => {
+    const result = findIntersects(
+      [
+        { x: 0, y: 0 },
+        { x: 8, y: 0 },
+        { x: 8, y: 5 },
+        { x: 3, y: 5 },
+        { x: 3, y: 2 }
+      ],
+      [
+        { x: 0, y: 0 },
+        { x: 0, y: 7 },
+        { x: 6, y: 7 },
+        { x: 6, y: 3 },
+        { x: 2, y: 3 }
+      ],
+    );
+    expect(result).toEqual([
+      { x: 6, y: 5 }, { x: 3, y: 3 }
+    ]);
+  });
+
+  it('returns the manhattan distance of the closest intersection', () => {
+    const result = calcDistanceOfClosestIntersect([{ x: 6, y: 5 }, { x: 3, y: 3 }]);
+    expect(result).toBe(6);
+  });
+
+  it('given two paths, it returns the distance of the closest intersection', () => {
+    const firstPath = 'R75,D30,R83,U83,L12,D49,R71,U7,L72';
+    const secondPath = 'U62,R66,U55,R34,D71,R55,D58,R83';
+    const result = crossedWires(firstPath, secondPath);
+    expect(result).toBe(159);
+
+    const firstPath2 = 'R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51';
+    const secondPath2 = 'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7';
+    const result2 = crossedWires(firstPath2, secondPath2);
+    expect(result2).toBe(135);
   });
 });
