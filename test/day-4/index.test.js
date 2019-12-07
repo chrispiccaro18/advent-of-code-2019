215ae98b4a2161583cc46cdfeeac9f6c78e23049
chrispiccaro <chrispiccaro18@gmail.com>
2019-12-07 14:57:38 -0800

    recursion

--- test/day-4/index.test.js
+++ test/day-4/index.test.js
@@ -1,14 +1,34 @@
 const {
+  combinedCheck,
   // findRange,
-  checkForAdjacentSame,
+  findGoodNumber,
+  digitsToNumber,
 } = require('../../lib/day-4/index');
 // const input = require('../../lib/day-4/input');
 
 describe('day 4', () => {
-  it('returns true for good number', () => {
-    expect(checkForAdjacentSame(122345)).toBe(true);
-    expect(checkForAdjacentSame(123456)).toBe(false);
-    expect(checkForAdjacentSame(123455)).toBe(true);
-    expect(checkForAdjacentSame(113456)).toBe(true);
+  const goodNumber = { doubles: true, digit: null };
+  it('checks for doubles and increasing digits', () => {
+    expect(combinedCheck(122345)).toEqual({
+      ...goodNumber,
+      digits: Array.from((122345).toString()).map(Number)
+    });
+    // expect(combinedCheck(123456)).toEqual({ doubles: false, digit: null, });
+    // expect(combinedCheck(123455)).toEqual(goodNumber);
+    // expect(combinedCheck(113456)).toEqual(goodNumber);
+    // expect(combinedCheck(122345)).toEqual(goodNumber);
+    // expect(combinedCheck(122454)).toEqual({ doubles: true, digit: 5, });
+    // expect(combinedCheck(123455)).toEqual(goodNumber);
+    // expect(combinedCheck(103456)).toEqual({ doubles: false, digit: 1, });
+  });
+  // it('returns starting range given input', () => {
+  //   expect(findRange(input)).toEqual([356666, 888888]);
+  // });
+  it('returns a good number', () => {
+    expect(findGoodNumber(111111)).toBe(111111);
+    expect(findGoodNumber(111110)).toBe(111111);
+  });
+  it('returns a number given digits', () => {
+    expect(digitsToNumber([1, 2, 3, 4, 5, 6])).toBe(123456);
   });
 });
